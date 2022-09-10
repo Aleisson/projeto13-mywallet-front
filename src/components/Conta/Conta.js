25d193ecf5e13d8dbad145f084dbf3bb45e2914b
Aleisson <caioaleisson@gmail.com>
2022-09-10 17:27:10 -0300

    design conta

--- src/components/Conta/Conta.js
+++ src/components/Conta/Conta.js
@@ -1,4 +1,6 @@
-import { ContaBody } from "./style";
+import { StyledLink, ContaBody, NavConta, HistoryConta, BottonConta, ButtonConta } from "./style";
+
+
 
 function Conta() {
 
@@ -6,7 +8,28 @@ function Conta() {
     return (
 
         <ContaBody>
-           
+            <NavConta><h2>Olá, Fulano</h2><ion-icon name="log-out-outline"></ion-icon></NavConta>
+            <HistoryConta>
+                <h1>
+                    <p>Não há registros de </p>
+                    <p>entrada ou saída</p>
+                </h1>
+
+            </HistoryConta>
+            <BottonConta>
+                <StyledLink to="/movimento">
+                    <ButtonConta>
+                        <ion-icon name="add-circle-outline"></ion-icon>
+                        <h4><p>Nova</p><p>entrada</p></h4>
+                    </ButtonConta>
+                </StyledLink>
+                <StyledLink to="/movimento">
+                    <ButtonConta>
+                        <ion-icon name="remove-circle-outline"></ion-icon>
+                        <h4><p>Nova</p><p>saída</p></h4>
+                    </ButtonConta>
+                </StyledLink>
+            </BottonConta>
         </ContaBody>
 
     )
--- src/components/Conta/style.js
+++ src/components/Conta/style.js
@@ -9,20 +9,108 @@ const StyledLink = styled(Link)`
 
 const ContaBody = styled.div`
 
-    background-color: crimson;
-    height: 180vw;
+    /* background-color: crimson; */
+    height: 182vw;
     width:100%;
     margin: 10px auto;
     display:flex;
     flex-direction:column;
     align-items:center;
     justify-content:space-around;
+    padding:4vw;
 
 `
 const NavConta = styled.div`
 
+        /* background-color:greenyellow; */
+        display:flex;
+        width: 100%;
+        height: 16vw;
+        padding:2px;
+        justify-content: space-between;
+        align-items:center;
         
+
+        h2{
+            font-family: 'Raleway';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 24px;
+            line-height: 31px;
+            color:#ffffff;
+        }
+
+        ion-icon{
+            font-size: 24px;
+            color:#ffffff
+        }
 
 `
 
-export {StyledLink, ContaBody};
+const HistoryConta = styled.div`
+
+    background: #FFFFFF;
+    border-radius: 5px;
+    width:100%;
+    height:  118vw;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:space-between;
+
+    h1{
+        font-family: 'Raleway';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 23px;
+        text-align: center;
+        color: #868686;
+        position:relative;
+        top:50%;
+        
+    }
+
+`
+
+const BottonConta = styled.div`
+
+
+/* background-color:#ffffff; */
+    width:100%;
+    height:24vw;
+    display:Flex;
+    justify-content:space-between;
+
+
+`
+
+const ButtonConta = styled.div`
+
+    background-color: #A328D6;
+    border-radius: 2.5px;
+    width: 42vw;
+    height:100%;
+    display:flex;
+    flex-direction:column;
+    align-items:flex-start;
+    justify-content:space-between;
+    padding:6px;
+
+    h4{
+        font-family: 'Raleway';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 20px;
+        color: #FFFFFF;
+    }
+
+    ion-icon{
+        color:#ffffff;
+    }
+
+`
+
+
+export { StyledLink, ContaBody, NavConta, HistoryConta, BottonConta, ButtonConta };
